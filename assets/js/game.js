662040b5647eb98e2a6194abedfb3e6dc8d7c57e
Bogdan933 <bogdan.gugu@hotmail.com>
2022-09-12 12:29:36 +0000

    JS: Add the first question with choices array and correct answer

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,3 +3,20 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressbarfull');
+
+let currentQuestion = {}
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQUestions = [];
+
+let Questions = [
+    {
+        question: 'What is 30 + 40?',
+        choice1: '10',
+        choice2: '100',
+        choice3: '55',
+        choice4: '70',
+        answer: 70,
+    }
+]
