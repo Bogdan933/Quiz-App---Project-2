702ca5c78e139d8086069fb4d47aead6d3c5f620
Bogdan933 <bogdan.gugu@hotmail.com>
2022-09-14 23:36:41 +0000

    BUG: When game is finished the save score page does not load and i get an 404 error, il worked on pathing but did not fix, will update

--- assets/js/end-game.js
+++ assets/js/end-game.js
@@ -1,9 +1,9 @@
 const username = document.querySelector('#username');
 const saveScoreBtn = document.querySelector('#saveScoreBtn');
 const finalScore = document.querySelector('#finalScore');
-const mostRecentScore = document.querySelector('#mostRecentScore');
+const mostRecentScore = localStorage.getItem('#mostRecentScore');
 
-const highScore = JSON.parse(localStorage.getItem('highScores')) || [];
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
 const MAX_HIGH_SCORES = 5;
 
@@ -14,7 +14,7 @@ username.addEventListener('keyup', () => {
 });
 
 saveHighScore = e => {
-    e.preventDefault ()
+    e.preventDefault()
 
     const score = {
         score: mostRecentScore,
