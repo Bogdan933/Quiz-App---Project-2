c88f7d720f787c322b02fb95ce4ffa7194b8a47e
Bogdan933 <bogdan.gugu@hotmail.com>
2022-09-15 12:42:30 +0000

    Issue still persists after changing all relative paths, i now changed the return location for endgame.html inside game.js line 58

--- assets/js/game.js
+++ assets/js/game.js
@@ -55,7 +55,7 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score);
 
-        return window.location.assign('/endgame.html');
+        return window.location.assign('endgame.html');
     }
 
     /* Incrementing by 1 each question */
